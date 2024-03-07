ab33ad3ceef7134588eebe7d98b641edcfd0a837
Violet Hall <violet.hall@kerv.com>
2024-03-07 10:02:49 +0000

    Example broken aws_cognito_resource_server
    Signed-off-by: Violet Hall <violet.hall@kerv.com>

---
+++ src/cognito.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_cognito_user_pool" "pool" {
+  name = "test-pool"
+}
+
+resource "aws_cognito_resource_server" "service_scopes" {
+  user_pool_id = aws_cognito_user_pool.pool.id
+  identifier   = "test-cognito"
+  name         = "test-cognito"
+
+  scope {
+    scope_name        = "SAMPLE-SCOPE"
+    scope_description = "Scope 1"
+  }
+  #   scope {
+  #     scope_name        = "MISSING-SCOPE"
+  #     scope_description = "Scope 2"
+  #   }
+}
+
+output "scopes" {
+  value = aws_cognito_resource_server.service_scopes.scope_identifiers
+}
